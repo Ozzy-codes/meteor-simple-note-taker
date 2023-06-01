770131a2c3da13996b66ff26289d4edb4adacfe8
Osvaldo Mesa <ossmesa@gmail.com>
2023-06-01 16:30:26 -0700

    touch up tailwind config syntax style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   // purge: ['./imports/ui/**/*.{js,jsx,ts,tsx}', './public/*.html'],
   content: [
-    "./imports/ui/*.{js,jsx,ts,tsx}",
-    "./imports/ui/*.html",
+    './imports/ui/*.{js,jsx,ts,tsx}',
+    './imports/ui/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ['Martel Sans', 'sans-serif'],
-      body: ['Martel Sans', 'sans-serif'],
+      body: ['Martel Sans', 'sans-serif']
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '112': '28rem',
-        '96': '24rem',
-        '80': '20rem',
-        '7': '1.875rem',
+        128: '32rem',
+        112: '28rem',
+        96: '24rem',
+        80: '20rem',
+        7: '1.875rem'
       },
       fontSize: {
         '4xs': '0.625rem',
@@ -35,4 +35,4 @@ module.exports = {
     extend: {}
   },
   plugins: []
-};
+}
