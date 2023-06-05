3a5b6609318e7d59384817a492f1c84ac8fea077
Mitch Colleran <mitch@joinit.org>
2023-06-05 23:29:49 +0100

    Connected to cloud atlas

--- imports/db/noteCollection.js
+++ imports/db/noteCollection.js
@@ -1,3 +1,6 @@
 import { Mongo } from 'meteor/mongo'
 
 export const NotesCollection = new Mongo.Collection('notes')
+
+// process.env.MONGO_URL = mongoURL;
+// YourCollection._connection = YourCollection.connect(mongoURL);
