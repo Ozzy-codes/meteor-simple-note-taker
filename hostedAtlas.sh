9b1af9243aeda8ad39de90488769d2fc01e8f4a1
Osvaldo Mesa <ossmesa@gmail.com>
2023-06-06 15:10:57 -0700

    add hosted db instance script

---
+++ hostedAtlas.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Required environment variables
+export MONGO_URL='mongodb+srv://simple-note-user:ephkMeXGuHsTy5Vq@cluster0.ntqhvbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+# Run meteor app
+meteor 
