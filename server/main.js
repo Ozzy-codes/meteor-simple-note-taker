a503d5f637dd7d7cb0ec2574280b64e6b7de0022
Osvaldo Mesa <ossmesa@gmail.com>
2023-06-07 14:45:21 -0700

    add default notes for each new user

--- server/main.js
+++ server/main.js
@@ -3,21 +3,3 @@ import { NotesCollection } from '../imports/db/noteCollection'
 
 import '../imports/api/noteMethods'
 import '../imports/api/notesPublications'
-
-const insertNote = (title, body) =>
-  NotesCollection.insert({
-    noteTitle: title,
-    noteBody: body,
-    createdAt: new Date()
-  })
-
-Meteor.startup(() => {
-  if (NotesCollection.find().count() === 0) {
-    [
-      { title: 'first note', body: 'This is the body for the first note' },
-      { title: 'second note', body: 'This is the body for the second note' },
-      { title: 'third note', body: 'This is the body for the third note' },
-      { title: 'fourth note', body: 'This is the body for the fourth note' }
-    ].forEach(note => insertNote(note.title, note.body))
-  }
-})
