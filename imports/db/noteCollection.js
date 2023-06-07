10ef86755292c2dc01d5ad19115a9a5678782e03
Osvaldo Mesa <ossmesa@gmail.com>
2023-06-07 10:48:08 -0700

    extract cloud-atlas from main branch

--- imports/db/noteCollection.js
+++ imports/db/noteCollection.js
@@ -1,6 +1,3 @@
 import { Mongo } from 'meteor/mongo'
 
 export const NotesCollection = new Mongo.Collection('notes')
-
-// process.env.MONGO_URL = mongoURL;
-// YourCollection._connection = YourCollection.connect(mongoURL);
--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor'
-import { Mongo } from 'meteor/mongo';
 import { NotesCollection } from '../imports/db/noteCollection'
 
 import '../imports/api/noteMethods'
@@ -13,24 +12,12 @@ const insertNote = (title, body) =>
   })
 
 Meteor.startup(() => {
-  
-  const url = Meteor.settings.mongo.url;
-
-  if (url) {
-    const db = new MongoInternals.RemoteCollectionDriver(url);
-    NotesCollection = new Mongo.Collection('NotesCollection', { _driver: db });
-  };
-
   if (NotesCollection.find().count() === 0) {
-
     [
       { title: 'first note', body: 'This is the body for the first note' },
       { title: 'second note', body: 'This is the body for the second note' },
       { title: 'third note', body: 'This is the body for the third note' },
       { title: 'fourth note', body: 'This is the body for the fourth note' }
     ].forEach(note => insertNote(note.title, note.body))
-  
   }
-
-});
-
+})
