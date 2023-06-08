88653dc45fc05709d4a9c7a14fca0d52b77d2a3f
Osvaldo Mesa <ossmesa@gmail.com>
2023-06-08 10:06:03 -0700

    add check for user sign in before publishing notes
    A error was throwing when loging out of a user due to a search for userId in the notes publication method.

--- imports/api/notesPublications.js
+++ imports/api/notesPublications.js
@@ -2,24 +2,25 @@ import { Meteor } from 'meteor/meteor'
 import { NotesCollection } from '../db/noteCollection'
 
 Meteor.publish('notes', function () {
-  const insertNote = (title, body) => {
-    const userEmail = Meteor.users.findOne(this.userId).emails[0].address
-
-    NotesCollection.insert({
-      noteTitle: title,
-      noteBody: body,
-      createdAt: new Date(),
-      userId: this.userId,
-      userEmail
-    })
+  if (this.userId) {
+    const insertNote = (title, body) => {
+      const userEmail = Meteor.users.findOne(this.userId).emails[0].address
+      NotesCollection.insert({
+        noteTitle: title,
+        noteBody: body,
+        createdAt: new Date(),
+        userId: this.userId,
+        userEmail
+      })
+    }
+    if (NotesCollection.find({ userId: this.userId }).count() === 0) {
+      [
+        { title: 'first note', body: 'This is the body for the first note' },
+        { title: 'second note', body: 'This is the body for the second note' },
+        { title: 'third note', body: 'This is the body for the third note' },
+        { title: 'fourth note', body: 'This is the body for the fourth note' }
+      ].forEach(note => insertNote(note.title, note.body))
+    }
+    return NotesCollection.find({ userId: this.userId }, { sort: { createdAt: -1 } })
   }
-  if (NotesCollection.find({ userId: this.userId }).count() === 0) {
-    [
-      { title: 'first note', body: 'This is the body for the first note' },
-      { title: 'second note', body: 'This is the body for the second note' },
-      { title: 'third note', body: 'This is the body for the third note' },
-      { title: 'fourth note', body: 'This is the body for the fourth note' }
-    ].forEach(note => insertNote(note.title, note.body))
-  }
-  return NotesCollection.find({ userId: this.userId }, { sort: { createdAt: -1 } })
 })
